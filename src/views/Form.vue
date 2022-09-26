58d6d9a56bb50c6371abcf53e508bef44da1b30e
walisonls <gwalisonls@gmail.com>
2022-09-26 09:39:01 -0300

    update

---
+++ src/views/Form.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mt-2">
+    <b-form>
+      <b-form-group
+      label="Titulo"
+      label-for="subject"
+      >
+      
+      </b-form-group>
+    </b-form>
+  </div>
+</template>
