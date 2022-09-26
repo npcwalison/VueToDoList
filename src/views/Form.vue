b4c90a3212d9f2b86faf0cd615989a0d51230756
walisonls <gwalisonls@gmail.com>
2022-09-26 10:33:40 -0300

    update

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,12 +1,65 @@
 <template>
   <div class="container mt-2">
     <b-form>
-      <b-form-group
-      label="Titulo"
-      label-for="subject"
-      >
-      
+      <b-form-group label="Title" label-for="subject">
+        <b-form-input
+          id="subject"
+          v-model="form.subject"
+          type="text"
+          placeholder="Ex: lava carro"
+          required
+          autocomplete="off"
+        >
+        </b-form-input>
       </b-form-group>
+
+      <b-form-group label="Descrição" label-for="description">
+        <b-form-textarea
+          id="description"
+          v-model="form.description"
+          type="text"
+          placeholder="Ex: levar o carro para lavar no fim de semana"
+          required
+          autocomplete="off"
+        >
+        </b-form-textarea>
+      </b-form-group>
+
+      <b-button type="submit" variant="outline-primary" @click="saveTask"> Salvar </b-button>
     </b-form>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "Form",
+    data() {
+      return {
+        form: {
+          subject: "",
+          description: "",
+        },
+        methodsSave: 'new'
+      };
+    },
+    created() {
+      if(this.$route.params.index === 0 || this.$route.params.index !== undefined) {
+        this.methodsSave = 'update'
+        let tasks = JSON.parse(localStorage.getItem("tasks"))
+        this.form =tasks[this.$route.params.index]
+      }
+    },
+    methods: {
+      saveTask() {
+        //verifica se há alguma task presente no localStorage, caso não haja nenhuma, ele retorna um array vazio
+        let tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : []
+        //adicionando tarefas do form no tasks, do tasks no localStorage
+        tasks.push(this.form)
+        //converte as tasks em string
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+        //redireciona para rota de lista
+        this.$router.push({ name: "list" })
+      }
+    }
+  };
+</script>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="container mt-2">
-    <b-card title="Titulo">
-      <b-card-text>descrição</b-card-text>
-      <b-button variant="outline-secondary" class="mr-2"> Editar </b-button>
-      <b-button variant="outline-danger" class="mr-2"> Excluir </b-button>
-    </b-card>
+  <div class="co(ntainer mt-2">
+    <div v-for="(task, index) in tasks" :key="index">
+      <b-card :title="task.subject">
+        <b-card-text>{{task.description}}</b-card-text>
+        <b-button variant="outline-secondary" @click="edit(index)" class="mr-2"> Editar </b-button>
+        <b-button variant="outline-danger" @click="" class="mr-2"> Excluir </b-button>
+      </b-card>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "List",
+    data() {
+      return {
+        tasks: []
+      };
+    },
+    created() {
+      this.tasks = (localStorage.getItem("tasks")) ? JSON.parse(localStorage.getItem("tasks")) : []
+    },
+    methods: {
+      edit(index) {
+        this.$router.push({ name: "form", params: { index } })
+      }
+    }
+  };
+</script>
+
